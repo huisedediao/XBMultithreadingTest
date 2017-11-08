55cbd5f19fa83401fbcd3f7f516420d6ff544a19
huisedediao <401758785@qq.com>
2017-11-08 11:33:05 +0800

    base commit

---
+++ XBSynchronizeTest/XBSynchronizeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBSynchronizeTest
+//
+//  Created by xxb on 2017/11/7.
+//  Copyright © 2017年 xxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
